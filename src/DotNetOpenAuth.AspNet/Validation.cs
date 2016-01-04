be9c4e044a92a1e4bab82b7f3bbbfb91d34c53db
Pent Ploompuu <kaalikas@gmail.com>
2016-01-04 16:42:11 +0200

    removed Validation dependency

---
+++ src/DotNetOpenAuth.AspNet/Validation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Validation
+{
+	static class Requires
+	{
+		public static void NotNull(object value, string paramName)
+		{
+			if(value == null) throw new ArgumentNullException(paramName);
+		}
+
+		public static void NotNullOrEmpty(string value, string paramName)
+		{
+			if(value == null) throw new ArgumentNullException(paramName);
+			if(value.Length == 0) throw new ArgumentException($"{paramName} must not be empty", paramName);
+		}
+
+		public static void NotNullOrEmpty(string[] value, string paramName)
+		{
+			if(value == null) throw new ArgumentNullException(paramName);
+			if(value.Length == 0) throw new ArgumentException($"{paramName} must not be empty", paramName);
+		}
+	}
+}
